c150ba084044e1ced91597f790ac821cc8cf8c4f
Elnur <mamekov-e@mail.ru>
2023-07-11 15:35:14 +0600

    Initial project setup

---
+++ src/main/java/kz/sitehealthtracker/site_health_tracker/SiteHealthTrackerApplication.java
@@ -0,0 +1,13 @@
+package kz.sitehealthtracker.site_health_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteHealthTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteHealthTrackerApplication.class, args);
+    }
+
+}
