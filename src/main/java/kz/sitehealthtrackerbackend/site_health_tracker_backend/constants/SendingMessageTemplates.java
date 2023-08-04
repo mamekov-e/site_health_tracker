d83c33b02a0c39c8cadae1fde7a25a92cbc27440
Elnur <mamekov-e@mail.ru>
2023-08-04 12:13:36 +0600

    Add sites with status list in notification msg

--- src/main/java/kz/sitehealthtrackerbackend/site_health_tracker_backend/constants/SendingMessageTemplates.java
+++ src/main/java/kz/sitehealthtrackerbackend/site_health_tracker_backend/constants/SendingMessageTemplates.java
@@ -1,8 +1,12 @@
 package kz.sitehealthtrackerbackend.site_health_tracker_backend.constants;
 
+import kz.sitehealthtrackerbackend.site_health_tracker_backend.model.Site;
 import kz.sitehealthtrackerbackend.site_health_tracker_backend.model.SiteGroup;
 import kz.sitehealthtrackerbackend.site_health_tracker_backend.web.dtos.SiteDto;
 
+import java.util.List;
+
+import static kz.sitehealthtrackerbackend.site_health_tracker_backend.constants.Delimiters.NEW_LINE;
 import static kz.sitehealthtrackerbackend.site_health_tracker_backend.constants.ExpireConstants.TELEGRAM_UNSUBSCRIBED_EXPIRATION_TIME;
 
 public class SendingMessageTemplates {
@@ -35,18 +39,34 @@ public class SendingMessageTemplates {
 
     public static final String GROUP_STATUS_CHANGED_NOTIFICATION_MESSAGE_CONTENT_TEMPLATE = "Привет, подписчик!<d><d>" +
             "Уведомляем Вас, что статус группы %s изменился: %s.<d>" +
-            "Причина: статус сайта %s изменился - %s.<d><d>" +
+            "Причина: сайт %s - %s.<d><d>" +
+            "Все сайты группы:<d>" +
+            "%s<d>" +
             "С уважением,<d>" +
             "Site Health Tracker team.";
+
+    private static final String SITE_ITERATION_TEXT = "%d. %s - %s";
 
     public static String groupStatusChangedTemplateWithDelimiter(SiteGroup siteGroup, Delimiters delimiter, SiteDto siteWithChangedStatus) {
         String groupName = siteGroup.getName();
         String newGroupStatus = siteGroup.getStatus().getStatusValue();
         String siteName = siteWithChangedStatus.getName();
         String siteStatus = siteWithChangedStatus.getStatus().getStatusValue();
+        List<Site> sitesOfGroup = siteGroup.getSites();
+        StringBuilder allGroupSites = new StringBuilder();
+
+        if (sitesOfGroup.isEmpty()) {
+            allGroupSites.append("Нет сайтов").append(NEW_LINE.getDELIMITER());
+        } else {
+            for (int i = 0; i < sitesOfGroup.size(); i++) {
+                Site site = sitesOfGroup.get(i);
+                String siteText = String.format(SITE_ITERATION_TEXT, i+1, site.getName(), site.getStatus().getStatusValue());
+                allGroupSites.append(siteText).append(NEW_LINE.getDELIMITER());
+            }
+        }
         String delimitersReplacedTemplate = GROUP_STATUS_CHANGED_NOTIFICATION_MESSAGE_CONTENT_TEMPLATE
                 .replaceAll(Delimiters.REPLACING_DELIMITER.DELIMITER, delimiter.DELIMITER);
 
-        return String.format(delimitersReplacedTemplate, groupName, newGroupStatus, siteName, siteStatus);
+        return String.format(delimitersReplacedTemplate, groupName, newGroupStatus, siteName, siteStatus, allGroupSites);
     }
 }
