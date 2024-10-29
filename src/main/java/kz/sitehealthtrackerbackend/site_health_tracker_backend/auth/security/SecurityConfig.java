c1cc9c6951ac943a12da73a14b15aa31a953a35a
e.mamekov <mamekov-e@mail.ru>
2024-10-29 19:56:23 +0500

    Update application-dev.yaml and config

--- src/main/java/kz/sitehealthtrackerbackend/site_health_tracker_backend/auth/security/SecurityConfig.java
+++ src/main/java/kz/sitehealthtrackerbackend/site_health_tracker_backend/auth/security/SecurityConfig.java
@@ -9,23 +9,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -58,11 +50,11 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // Allow localhost:3000
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")  // Allow all headers
-                .exposedHeaders("Authorization")  // Expose Authorization header to frontend
-                .allowCredentials(false);  // Allow credentials (cookies, authorization headers)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(false);
     }
 
     @Bean
@@ -83,12 +75,4 @@ public class SecurityConfig implements WebMvcConfigurer {
         return daoAuthenticationProvider;
     }
 
-}
-
-class Dog {
-    private String name;
-
-    public Dog(String name) {
-        name = name;
-    }
 }
